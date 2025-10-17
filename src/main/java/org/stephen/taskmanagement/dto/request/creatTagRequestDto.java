5f0f7c2c67bdfdac9e457161ffd3c828456dcba0
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-10-17 17:08:27 +0300

    wrote creatTagRequestDto

---
+++ src/main/java/org/stephen/taskmanagement/dto/request/creatTagRequestDto.java
@@ -0,0 +1,21 @@
+package org.stephen.taskmanagement.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class creatTagRequestDto {
+
+    @NotBlank(message = "Tag name is required")
+    @Size(min = 1, max = 100, message = "Tag name must be between 1 and 100 characters")
+    private String name;
+
+    @Size(max = 500, message = "Description must not exceed 500 characters")
+    private String description;
+
+}
