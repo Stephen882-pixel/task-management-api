b8c96c4000eceece4748eded67f2f88d07422c1f
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-10-17 17:09:56 +0300

    wrote UpdateTagRequestDto

---
+++ src/main/java/org/stephen/taskmanagement/dto/request/UpdateTagRequestDto.java
@@ -0,0 +1,18 @@
+package org.stephen.taskmanagement.dto.request;
+
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateTagRequestDto {
+    @Size(min = 1, max = 100, message = "Tag name must be between 1 and 100 characters")
+    private String name;
+
+    @Size(max = 500, message = "Description must not exceed 500 characters")
+    private String description;
+
+}
