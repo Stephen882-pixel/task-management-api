626b6766fbaa6d1fc6792e6015253820f3f38fec
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-10-17 20:09:16 +0300

    task controller - create task

--- src/main/java/org/stephen/taskmanagement/controller/TaskController.java
+++ src/main/java/org/stephen/taskmanagement/controller/TaskController.java
@@ -1,4 +1,30 @@
 package org.stephen.taskmanagement.controller;
 
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.stephen.taskmanagement.dto.request.CreateTaskRequestDto;
+import org.stephen.taskmanagement.dto.response.CreateTaskResponseDto;
+import org.stephen.taskmanagement.service.TaskService;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+@RequiredArgsConstructor
+@Slf4j
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public ResponseEntity<CreateTaskResponseDto> createTask(@Valid @RequestBody CreateTaskRequestDto request){
+        log.info("POST /api/v1/tasks - Creating new task");
+        CreateTaskResponseDto response = taskService.createTask(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 }
