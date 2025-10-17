512fe13c01e14a4114a3b87d1351a60a778506f8
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-10-17 17:02:35 +0300

    wrote update task request dto

---
+++ src/main/java/org/stephen/taskmanagement/dto/request/updateTaskRequestDto.java
@@ -0,0 +1,27 @@
+package org.stephen.taskmanagement.dto.request;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class updateTaskRequestDto {
+
+    @Size(min = 1, max = 255, message = "Title must be between 1 and 255 characters")
+    private String title;
+
+    @Size(max = 5000, message = "Description must not exceed 5000 characters")
+    private String description;
+
+    @FutureOrPresent(message = "Due date must be in the future or present")
+    private LocalDateTime dueDate;
+    private String status;
+    private Set<String> tagNames;
+}
